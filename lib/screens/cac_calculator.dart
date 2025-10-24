d79f763db230853d5353ac741d164780fac25fa0
zabihullah05 <zabihbhai62@gmail.com>
2025-10-24 09:20:36 +0500

    Update cac_calculator.dart

--- lib/screens/cac_calculator.dart
+++ lib/screens/cac_calculator.dart
@@ -28,7 +28,7 @@ class _CACCalculatorState extends State<CACCalculator> {
 
   void _downloadPDF() {
     if (_cac != null) {
-      PDFService.generateAndDownload(
+      pdfService.generateAndDownload(
         title: "CAC Calculator Result",
         content:
             "Customer Acquisition Cost (CAC): \$${_cac!.toStringAsFixed(2)}",
