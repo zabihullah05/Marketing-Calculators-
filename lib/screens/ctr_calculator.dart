4994d9d9f2e27b81d895a960e9f15310ab80355b
zabihullah05 <zabihbhai62@gmail.com>
2025-10-24 09:25:01 +0500

    Update ctr_calculator.dart

--- lib/screens/ctr_calculator.dart
+++ lib/screens/ctr_calculator.dart
@@ -23,7 +23,7 @@ class _CTRCalculatorState extends State<CTRCalculator> {
 
   void _downloadPDF() {
     if (_ctr != null) {
-      PDFService.generateAndDownload(
+      pdfService.generateAndDownload(
         title: "CTR Calculator Result",
         content: "Click Through Rate: ${_ctr!.toStringAsFixed(2)}%",
       );
